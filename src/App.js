403631ffb9081e52b09937cae2e0df0a835b907a
emyrue <94935633+emyrue@users.noreply.github.com>
2022-04-26 11:08:08 -0600

    Home page created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
 import './App.css';
+import Home from './components/home';
 import Calculator from './components/calculator';
 
 class App extends React.Component {
@@ -9,7 +13,19 @@ class App extends React.Component {
   }
 
   render() {
-    return (<Calculator previous="" operand="" current="0" solved={false} />);
+    return (
+      <Router>
+        <nav>
+          <Link to="/"> Home </Link>
+          <Link to="/calculator"> Calculator </Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator previous="" operand="" current="0" solved={false} />} />
+          <Route path="*" element={<div>Page not found</div>} />
+        </Routes>
+      </Router>
+    );
   }
 }
 
